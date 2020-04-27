ca76720127d40903b13c648d887a3dea1884e26b
MHanSi Senanayake <39160365+mhansi@users.noreply.github.com>
2020-04-28 00:46:59 +0530

    Created repo

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div class="ui fixed inverted menu vue-color">
+    <div class="ui container">
+      <a href="#" class="header item">Vue JS CRUD with Laravel API</a>
+    </div>
+    </div>
+    <div class="ui main container">
+      <Myform/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Myform from "./components/Myform";
+export default {
+  name: 'App',
+  components: {
+    Myform
+  }
+}
+</script>
+
+<style>
+.vue-color{
+  background:#41b883 !important;
+}
+
+.main.container{
+  margin-top:58px !important;
+ 
+}
+
+.submit-button{
+  margin-top: 24px !important;
+  float:right;
+}
+</style>
---
+++ src/components/Myform.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="my-form">
+      <form class="ui form">
+          <div class="fields">
+              <div class="four wide field">
+                  <label>First Name</label>
+                  <input type="text" name="first_name"/>
+              </div>
+
+               <div class="four wide field">
+                  <label>Last Name</label>
+                  <input type="text" name="last_name"/>
+              </div>
+
+               <div class="four wide field">
+                  <label>E-mail</label>
+                  <input type="email" name="joe@gmail.com"/>
+              </div>
+
+              <div class="two wide field">
+                  <button class="ui primary button submit-button">Save</button>
+              </div>
+          </div>
+      </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Myform"
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
